6f91064d58a8f8de390bae9078ed0e4881e85237
Aleksandrovich-Anastasia <78806033+Aleksandrovich-Anastasia@users.noreply.github.com>
2025-07-19 16:39:26 +0000

    Deploying to gh-pages from @ Aleksandrovich-Anastasia/goit-js-hw-11@eca8ed9075c6df0a291c89ee7259256c091e2fdd 🚀

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 import{a as f,S as m,i as n}from"./assets/vendor-rOMtvQ2t.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();const g="51362308-fccd9ac64991a037e5dade233",h="https://pixabay.com/api/";async function y(a){return(await f.get(h,{params:{key:g,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0}})).data}const l=document.querySelector(".gallery"),u=document.querySelector(".loader"),v=new m(".gallery a");function L(a){const t=a.map(({webformatURL:r,largeImageURL:i,tags:e,likes:s,views:o,comments:d,downloads:p})=>`
-      <a class="gallery-item" href="${i}">
-  <img src="${r}" alt="${e}" loading="lazy" />
-  <div class="info">
-    <div class="info-item">
-      <span class="label">Likes</span>
-      <span class="value">${s}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">Views</span>
-      <span class="value">${o}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">Comments</span>
-      <span class="value">${d}</span>
-    </div>
-    <div class="info-item">
-      <span class="label">Downloads</span>
-      <span class="value">${p}</span>
-    </div>
-  </div>
-</a>
+      <li class="gallery-item">
+        <a href="${i}">
+          <img src="${r}" alt="${e}" loading="lazy" />
+          <div class="info">
+            <div class="info-item">
+              <span class="label">Likes</span>
+              <span class="value">${s}</span>
+            </div>
+            <div class="info-item">
+              <span class="label">Views</span>
+              <span class="value">${o}</span>
+            </div>
+            <div class="info-item">
+              <span class="label">Comments</span>
+              <span class="value">${d}</span>
+            </div>
+            <div class="info-item">
+              <span class="label">Downloads</span>
+              <span class="value">${p}</span>
+            </div>
+          </div>
+        </a>
+      </li>
     `).join("");l.insertAdjacentHTML("beforeend",t),v.refresh()}function b(){l.innerHTML=""}function w(){u.classList.add("is-visible")}function x(){u.classList.remove("is-visible")}const c="/goit-js-hw-11/assets/sprite-BWBAyIsh.svg",$=document.querySelector(".form"),q=document.querySelector('input[name="search-text"]');$.addEventListener("submit",async a=>{a.preventDefault();const t=q.value.trim();if(!t){n.error({message:`
         <svg class="toast-icon" width="24" height="24">
           <use xlink:href="${c}#icon-error"></use>
